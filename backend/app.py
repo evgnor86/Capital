e2bf1f7a27cd2db6b3b50ffdefdc4fc0c163c58e
evgnor86 <atxn@yandex.ru>
2024-09-03 20:21:22 +0700

    1

--- backend/app.py
+++ backend/app.py
@@ -12,10 +12,6 @@ get_err_msg = lambda e: json.loads(e.json())[0]['msg']
 db_session = Session('salaries', YearSalary)
 _data = db_session.read()
 
-# _data = _data.years[2024]
-# _data = _data.years[2024].months[1]
-# print(f'{_data.model_dump_json(indent=2)}') 
-
 for year in (2024,):
     for month in (10, 11, 12,):
         print(f'{_data.years[year].months[month].model_dump_json(indent=2)}')
